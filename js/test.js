deab7d60a772905cc1874dc6ce17aef737e18255
concolele <thanhvinhtr3@gmail.com>
2018-05-16 17:06:57 +0700

    responsive1

--- js/test.js
+++ js/test.js
@@ -4,35 +4,10 @@
 // ' 作成日：2018/04/11 ... Vinh
 // ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-var clickMenuHeader = true;//sub-menu-content of down-menu
-var clickMenu = true;//sub-menu-content of down-menu
+$(document).ready(function(){
+	$('.menu-mobile').on('click', onClickMenu);
+});
 
-function clickMenuMobile(id){	
-	if(clickMenuHeader){
-		document.getElementById(id).style.display = "inline-block";
-		clickMenuHeader = false;
-	}else{
-		document.getElementById(id).style.display = "none";
-		clickMenuHeader = true;
-	}
-}
-
-function dropdownMobile(id){	
-	if(clickMenu){
-		document.getElementById(id).style.display = "inline-block";
-		clickMenu = false;
-	}else{
-		document.getElementById(id).style.display = "none";
-		clickMenu = true;
-	}
-}
-
-var $lba = document.getElementsByClassName('click-menu');
-for(var i=0; i < $lba.length; i++) $lba[i].onclick = function() {
-    clickMenuMobile("main-menu");
-}
-
-var $lba1 = document.getElementsByClassName('down-menu');
-for(var i=0; i < $lba1.length; i++) $lba1[i].onclick = function() {
-    dropdownMobile("sub-menu-content");
+function onClickMenu(){
+	$(this).toggleClass('open');
 }
